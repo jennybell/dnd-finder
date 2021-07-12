b113a45cd762c105d1aba30c81cef54199594689
Charlie Casserly <72042975+charlie-casserly@users.noreply.github.com>
2021-07-12 15:47:32 +0100

    Revert "User accounts"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
