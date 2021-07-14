5a32bcaf51a387be41def8fa7710768c2da4b3ce
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-14 12:34:20 +0100

    added avatar functionality with default image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_for(user)
+    if user.avatar.attached?
+      user.avatar
+    else
+      'default_avatar.jpg'
+    end
+  end
 end
--- spec/feature/a_signed_in_user_can_make_a_group_spec.rb
+++ spec/feature/a_signed_in_user_can_make_a_group_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 feature 'Groups' do 
   scenario 'a signed in user can create a group' do 
-    visit '/' 
-    click_link 'Sign up'
-    fill_in 'Email', with: 'test@example.com' 
-    fill_in 'Password', with: 'password123' 
-    fill_in 'Password confirmation', with: 'password123' 
-    click_button 'Sign up'
+    sign_up
     click_link 'Taverns'
     click_link 'New Group'
     fill_in 'Name', with: 'test group'
