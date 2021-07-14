57c58b744fa5a70b20e7ce71702f6180f17d832b
charlie-casserly <charliecasserly215@gmail.com>
2021-07-14 15:00:37 +0100

    test helpers used for old feature tests

--- spec/features/a_signed_in_user_can_make_a_group_spec.rb
+++ spec/features/a_signed_in_user_can_make_a_group_spec.rb
@@ -2,14 +2,7 @@ require 'rails_helper'
 
 feature 'Groups' do 
   scenario 'user can create a group' do 
-    visit '/' 
-    click_link 'Sign up'
-    fill_in 'Email', with: 'test@example.com'
-    fill_in 'Username', with: 'user 1'  
-    fill_in 'Password', with: 'password123' 
-    fill_in 'Password confirmation', with: 'password123' 
-    fill_in 'Location', with: "London"
-    click_button 'Sign up'
+    sign_up
     click_link 'Taverns'
     click_link 'New Group'
     fill_in 'Name', with: 'test group'
@@ -18,31 +11,11 @@ feature 'Groups' do
   end
 
   scenario 'other users can join groups' do
-    visit '/' 
-    click_link 'Sign up'
-    fill_in 'Email', with: 'test1@example.com' 
-    fill_in 'Username', with: 'user 1' 
-    fill_in 'Password', with: 'password123' 
-    fill_in 'Password confirmation', with: 'password123' 
-    fill_in 'Location', with: "London"
-    click_button 'Sign up'
-    click_link 'Taverns'
-    click_link 'New Group'
-    fill_in 'Name', with: 'test group'
-    click_button 'Create Group'
-    visit '/' 
-    click_link 'Sign Out'
-    click_link 'Sign up'
-    fill_in 'Email', with: 'test2@example.com'
-    fill_in 'Username', with: 'user 2' 
-    fill_in 'Password', with: 'password123' 
-    fill_in 'Password confirmation', with: 'password123' 
-    fill_in 'Location', with: "London"
-    click_button 'Sign up'
+    sign_up_create_group_and_sign_up_2nd_user
     click_link 'Taverns'
     click_link 'Show'
     click_button 'Join'
-    expect(page).to have_content 'user 1'
-    expect(page).to have_content 'user 2'
+    expect(page).to have_content 'testuser'
+    expect(page).to have_content 'testuser2'
   end   
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -12,6 +12,31 @@ def sign_up
   click_button "Sign up"
 end
 
+def sign_up_2nd_user 
+  visit("/users/sign_up")
+  fill_in 'Email', with: 'test2@example.com' 
+  fill_in 'Username', with: 'testuser2'
+  select 'Neutral', from: 'Alignment'
+  fill_in 'Location', with: 'London'
+  fill_in "Bio", with: 'testing profile'
+  check "user_dm"
+  fill_in 'Password', with: 'password123' 
+  fill_in 'Password confirmation', with: 'password123'
+  check "user_terms"
+  click_button "Sign up"
+end
+
+def sign_up_create_group_and_sign_up_2nd_user
+  sign_up
+  click_link 'Taverns'
+  click_link 'New Group'
+  fill_in 'Name', with: 'test group'
+  click_button 'Create Group'
+  visit '/' 
+  click_link 'Sign Out'
+  sign_up_2nd_user
+end
+
 def log_in
   fill_in 'Email', with: 'test@example.com'
   fill_in 'Password', with: 'password123'
