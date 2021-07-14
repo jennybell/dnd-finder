d418d11fbf04d326e2021f52ffa3ac745053bc48
charlie-casserly <charliecasserly215@gmail.com>
2021-07-14 12:34:52 +0100

    users can join groups

--- spec/feature/a_signed_in_user_can_make_a_group_spec.rb
+++ spec/feature/a_signed_in_user_can_make_a_group_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 feature 'Groups' do 
-  scenario 'a signed in user can create a group' do 
+  scenario 'user can create a group' do 
     visit '/' 
     click_link 'Sign up'
-    fill_in 'Email', with: 'test@example.com' 
+    fill_in 'Email', with: 'test@example.com'
+    fill_in 'Username', with: 'user 1'  
     fill_in 'Password', with: 'password123' 
     fill_in 'Password confirmation', with: 'password123' 
+    fill_in 'Location', with: "London"
     click_button 'Sign up'
     click_link 'Taverns'
     click_link 'New Group'
@@ -14,4 +16,33 @@ feature 'Groups' do
     click_button 'Create Group'
     expect(page).to have_content 'test group'
   end
+
+  scenario 'other users can join groups' do
+    visit '/' 
+    click_link 'Sign up'
+    fill_in 'Email', with: 'test1@example.com' 
+    fill_in 'Username', with: 'user 1' 
+    fill_in 'Password', with: 'password123' 
+    fill_in 'Password confirmation', with: 'password123' 
+    fill_in 'Location', with: "London"
+    click_button 'Sign up'
+    click_link 'Taverns'
+    click_link 'New Group'
+    fill_in 'Name', with: 'test group'
+    click_button 'Create Group'
+    visit '/' 
+    click_link 'Sign Out'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'test2@example.com'
+    fill_in 'Username', with: 'user 2' 
+    fill_in 'Password', with: 'password123' 
+    fill_in 'Password confirmation', with: 'password123' 
+    fill_in 'Location', with: "London"
+    click_button 'Sign up'
+    click_link 'Taverns'
+    click_link 'Show'
+    click_button 'Join'
+    expect(page).to have_content 'user 1'
+    expect(page).to have_content 'user 2'
+  end   
 end
