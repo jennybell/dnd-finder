bcc8262f19d6d4175c7084df76a12f62758ede9d
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-15 14:16:41 +0100

    added games migrations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,9 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_user
   has_many :messages
+<<<<<<< HEAD
   has_many :invitations
+=======
+  has_many :games
+>>>>>>> c8f1fc1 (added games migrations)
 end
