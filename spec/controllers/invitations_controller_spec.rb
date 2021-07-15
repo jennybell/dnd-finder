179078c0a61c54f22606697586c20f97032cdc19
charlie-casserly <charliecasserly215@gmail.com>
2021-07-15 13:11:17 +0100

    feature test written

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe InvitationsController, type: :controller do
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
+  
   end
 
 end
