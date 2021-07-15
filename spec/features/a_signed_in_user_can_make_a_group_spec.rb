1bd697dfa28b7df28fb00a553c77a54260a1a2b9
charlie-casserly <charliecasserly215@gmail.com>
2021-07-15 09:03:08 +0100

    group chat ibase functionality added

--- spec/features/a_signed_in_user_can_make_a_group_spec.rb
+++ spec/features/a_signed_in_user_can_make_a_group_spec.rb
@@ -9,13 +9,4 @@ feature 'Groups' do
     click_button 'Create Group'
     expect(page).to have_content 'test group'
   end
-
-  scenario 'other users can join groups' do
-    sign_up_create_group_and_sign_up_2nd_user
-    click_link 'Taverns'
-    click_link 'Show'
-    click_button 'Join'
-    expect(page).to have_content 'testuser'
-    expect(page).to have_content 'testuser2'
-  end   
 end
