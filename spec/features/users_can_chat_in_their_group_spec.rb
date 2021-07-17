84dbb737a703ea5c95b6b72114687eb8991e0589
charlie-casserly <charliecasserly215@gmail.com>
2021-07-17 16:48:14 +0100

    added a test for chat not being visible to non-members

--- spec/features/users_can_chat_in_their_group_spec.rb
+++ spec/features/users_can_chat_in_their_group_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 describe 'Group Chat' do 
-  scenario 'a user can send messages in their group' do 
+  scenario 'a group member can send messages in their group' do 
     sign_up
-    visit '/groups'
-    click_link 'New Group'
-    fill_in 'Name', with: 'test group'
-    click_button 'Create Group'
+    create_group
     fill_in('content', with: 'Sup JP and Willoughby and Jenny')
     click_button 'Send'
     expect(page).to have_content 'Sup JP and Willoughby and Jenny'
+  end
+
+  scenario 'a user cannot see chat if they are not a group member' do 
+    sign_up
+    create_group
+    log_out
+    sign_up_2nd_user
+    enter_group
+    expect(page).to_not have_content 'Add message:'
   end
 end
