686d5c2475fc56aef522092b58a3f4be0027a8c2
charlie-casserly <charliecasserly215@gmail.com>
2021-07-19 16:02:49 +0100

    information migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_134426) do
+ActiveRecord::Schema.define(version: 2021_07_19_150200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_134426) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "party_size"
+    t.text "information"
   end
 
   create_table "invitations", force: :cascade do |t|
