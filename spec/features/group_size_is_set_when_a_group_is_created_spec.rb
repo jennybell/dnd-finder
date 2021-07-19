0d9c10cce2398b98c5708d4e872da564f7ae7f9e
charlie-casserly <charliecasserly215@gmail.com>
2021-07-19 12:28:19 +0100

    party sizes added to database and views

--- spec/features/group_size_is_set_when_a_group_is_created_spec.rb
+++ spec/features/group_size_is_set_when_a_group_is_created_spec.rb
@@ -29,5 +29,23 @@ feature 'Group Size' do
     expect(page).to have_content 'Party Size: 2/10'
   end
 
-  
+  scenario 'users cannot request to join a full game' do 
+    sign_up
+    click_link 'Taverns'
+    click_link 'New Group'
+    fill_in 'Name', with: 'test group'
+    select "2", from: 'Party size'
+    click_button "Create Group"
+    log_out
+    sign_up_2nd_user
+    send_group_request
+    log_out
+    log_in
+    enter_group
+    click_link 'Accept'
+    log_out
+    sign_up_3rd_user
+    enter_group 
+    expect(page).not_to have_button 'Request'
+  end
 end 
