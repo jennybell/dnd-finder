b2154cc13a9935e5c0dab622872006ea35d20755
charlie-casserly <charliecasserly215@gmail.com>
2021-07-19 16:19:52 +0100

    test written for dms being able to update game information

--- spec/features/about_me_in_groups_spec.rb
+++ spec/features/about_me_in_groups_spec.rb
@@ -6,4 +6,23 @@ feature 'About Me' do
     create_group
     expect(page).to have_content 'Hello noobs!'
   end
+
+  scenario 'a DM can update game information' do 
+    sign_up
+    create_group
+    log_out 
+    sign_up_2nd_user
+    send_group_request
+    log_out
+    log_in
+    accept_group_request
+    click_link 'Assign DM'
+    log_out
+    sign_in_2nd_user
+    enter_group
+    click_link 'Edit Game Information'
+    fill_in 'Information', with: 'blaaaaah'
+    click_button 'Submit'
+    expect(page).to have_content 'blaaaaah'
+  end
 end
