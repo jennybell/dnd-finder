3674e186e0804c628669948c8fe4a99421448b77
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-20 16:41:23 +0100

    users can't click going button twice and can unclick WIP

--- app/controllers/game_responds_controller.rb
+++ app/controllers/game_responds_controller.rb
@@ -1,4 +1,5 @@
 class GameRespondsController < ApplicationController
+  
   def index
     
   end
@@ -9,5 +10,13 @@ class GameRespondsController < ApplicationController
     @game_respond = @game.game_responds.create(user_id: current_user.id)
     @game_respond.save!
     redirect_to group_path(@group)
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @game = Game.find(params[:game_id])
+    @game_respond = GameRespond.where(params[:id])
+    p @game
+    @game_respond.destroy
   end
 end
