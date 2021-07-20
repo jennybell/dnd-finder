7e073c526fa4c390e2766a7deb8dbfdd18baa2eb
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-20 16:41:23 +0100

    users can't click going button twice and can unclick WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_07_20_094545) do
-=======
 ActiveRecord::Schema.define(version: 2021_07_20_114808) do
->>>>>>> can add game edition to group information
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
