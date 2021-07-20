0315c74c8784d08c957a69a3705f68b61309d3cc
Jenny <jennybell42@gmail.com>
2021-07-20 11:07:32 +0100

    tests passing

--- spec/features/the_group_admin_can_schedule_a_game_spec.rb
+++ spec/features/the_group_admin_can_schedule_a_game_spec.rb
@@ -2,11 +2,13 @@ feature "schedule games" do
   scenario "the group admin can create a game" do
     sign_up
     create_group
+    fill_in 'Name', with: 'Battle'
     fill_in 'Date', with: '24/07/2021'
     fill_in 'Time', with: '18:00'
     click_button 'Schedule game'
     expect(page).to have_content('2021-07-24')
     expect(page).to have_content('18:00')
+    expect(page).to have_content('Battle')
   end
 
   scenario "users who aren't group admin cannot schedule a game" do
