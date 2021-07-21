4a066cdddfa9ae77e1051e2c81f9fe1e4978b396
JP Ferreira <jpferreira@MA301.broadband>
2021-07-21 19:33:49 +0100

    Styling on group page complete

--- spec/features/users_can_rsvp_to_a_game_spec.rb
+++ spec/features/users_can_rsvp_to_a_game_spec.rb
@@ -1,26 +1,26 @@
 
-# feature "group members can rsvp to a game" do
-#   scenario "members can respond to a game to say they are going" do
-#     sign_up
-#     create_group
-#     schedule_game
-#     click_link "Going"
-#     expect(page).to have_content "1 Going"
-#   end
+feature "group members can rsvp to a game" do
+  scenario "members can respond to a game to say they are going" do
+    sign_up
+    create_group
+    schedule_game
+    click_link "Going"
+    expect(page).to have_content "1 Going"
+  end
 
-#   scenario "it will say 0 going if no user has clicked the going link" do
-#     sign_up
-#     create_group
-#     schedule_game
-#     expect(page).to have_content "0 Going"
-#   end
+  scenario "it will say 0 going if no user has clicked the going link" do
+    sign_up
+    create_group
+    schedule_game
+    expect(page).to have_content "0 Going"
+  end
 
-#   scenario "the username will appear when clicking the going link" do
-#     sign_up
-#     create_group
-#     schedule_game
-#     click_link "Going"
-#     expect(page).to have_content "testuser"
-#   end
-# end
+  scenario "the username will appear when clicking the going link" do
+    sign_up
+    create_group
+    schedule_game
+    click_link "Going"
+    expect(page).to have_content "testuser"
+  end
+end
 
