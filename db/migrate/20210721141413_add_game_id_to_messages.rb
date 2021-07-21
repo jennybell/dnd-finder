17842f4c9f9cf928cc4b1d49589125aa8b3fe4c1
Jenny <jennybell42@gmail.com>
2021-07-21 16:41:15 +0100

    fix

--- db/migrate/20210721141413_add_game_id_to_messages.rb
+++ db/migrate/20210721141413_add_game_id_to_messages.rb
@@ -1,5 +1,5 @@
 class AddGameIdToMessages < ActiveRecord::Migration[6.1]
   def change
-    add_reference :messages, :games, null: false, foreign_key: true
+    add_reference :messages, :games, null: true, foreign_key: true
   end
 end
