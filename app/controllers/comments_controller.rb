76b526b3b6c186bc572ddcb78db5041ec284c0dd
JP Ferreira <jpferreira@MA301.broadband>
2021-07-21 15:16:49 +0100

    styling of group page complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
 
   def create
+    @group = Group.find(params[:group_id])
     @message = Message.find(params[:message_id])
     @comment = @message.comments.create(params[:comment].permit(:name, :body))
-    redirect_to groups_path
+    redirect_to group_path(@group)
   end
 
   def destroy
